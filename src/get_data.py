922d67211fd5b9cbce662c63aaf1337ba32ce9e6
Girija Shankar Vajrapu <vajrapushankar@gmail.com>
2023-08-15 18:55:18 +0530

    stage 1 complete and correct params file

--- src/get_data.py
+++ src/get_data.py
@@ -18,6 +18,8 @@ def get_data(config_path):
     df = pd.read_csv(data_path, sep=",", encoding='utf-8')
     return df
 
+# extra comment
+
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
