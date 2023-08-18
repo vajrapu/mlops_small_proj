fb705b219fd9fbf6ae10ebb7a0b589a99c73dfd0
Girija Shankar Vajrapu <vajrapushankar@gmail.com>
2023-08-18 11:47:12 +0530

    add not in range error class with pytest example

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,14 @@
+import pytest
+
+class NotInRangeError(Exception):
+    def __init__(self, message = "value not in range"):
+#        self.input_ = input_
+        self.message = message
+        super().__init__(self.message)
 
 
 def test_generic():
-    a = 2
-    b = 2
-    assert a == b
+    a = 5
+    with pytest.raises(NotInRangeError):
+        if a not in range(10,20):
+            raise NotInRangeError
