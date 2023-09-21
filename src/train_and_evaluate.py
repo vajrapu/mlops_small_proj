9edee8c046f418675e20bc8c4c0f6f61ad0f073f
Girija Shankar Vajrapu <vajrapushankar@pop-os.localdomain>
2023-09-21 16:35:06 +0530

    debug mlflow workflow

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -1,7 +1,6 @@
-# Load the train and test files
-# Train the algorithm
-# Save the metrics, params
-
+# load the train and test
+# train algo
+# save the metrices, params
 import os
 import warnings
 import sys
@@ -17,20 +16,18 @@ import joblib
 import json
 import mlflow
 
-
-def eval_metrics(actual, prediction):
-    rmse = np.sqrt(mean_squared_error(actual, prediction))
-    mae = mean_absolute_error(actual, prediction)
-    r2 = r2_score(actual, prediction)
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
     return rmse, mae, r2
-
 
 def train_and_evaluate(config_path):
     config = read_params(config_path)
     test_data_path = config["split_data"]["test_path"]
     train_data_path = config["split_data"]["train_path"]
     random_state = config["base"]["random_state"]
-    model_dir = config["mode_dir"]
+    model_dir = config["model_dir"]
 
     alpha = config["estimators"]["ElasticNet"]["params"]["alpha"]
     l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
@@ -46,32 +43,31 @@ def train_and_evaluate(config_path):
     train_x = train.drop(target, axis=1)
     test_x = test.drop(target, axis=1)
 
-############################# ML FLOW #####################################
+################### MLFLOW ###############################
     mlflow_config = config["mlflow_config"]
     remote_server_uri = mlflow_config["remote_server_uri"]
 
     mlflow.set_tracking_uri(remote_server_uri)
-    
+
     mlflow.set_experiment(mlflow_config["experiment_name"])
 
     with mlflow.start_run(run_name=mlflow_config["run_name"]) as mlops_run:
         lr = ElasticNet(
-            alpha=alpha,
-            l1_ratio=l1_ratio,
+            alpha=alpha, 
+            l1_ratio=l1_ratio, 
             random_state=random_state)
-
         lr.fit(train_x, train_y)
 
         predicted_qualities = lr.predict(test_x)
-
+        
         (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
         mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1 ratio", l1_ratio)
+        mlflow.log_param("l1_ratio", l1_ratio)
 
-        mlflow.log_metric("RMSE", rmse)
-        mlflow.log_metric("MAE", mae)
-        mlflow.log_metric("R2", r2)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2", r2)
 
         tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
 
@@ -82,9 +78,11 @@ def train_and_evaluate(config_path):
                 registered_model_name=mlflow_config["registered_model_name"])
         else:
             mlflow.sklearn.load_model(lr, "model")
+ 
 
-    if __name__ == "__main__":
-        args = argparse.ArgumentParser()
-        args.add_argument("--config", default="params.yaml")
-        parsed_args = args.parse_args()
-        train_and_evaluate(config_path=parsed_args.config)
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    train_and_evaluate(config_path=parsed_args.config)
